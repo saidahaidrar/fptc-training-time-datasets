78b32e1214d3e4080270f72bae25d8aca2e9a238
saidahaidrar <saidahaidrar@gmail.com>
2026-04-20 19:50:04 +0100

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import numpy as np
 
+def clean_phishing(input_path, output_path):
+    df = pd.read_csv(input_path)
+
+    df.replace([np.inf, -np.inf], np.nan, inplace=True)
+    df.dropna(inplace=True)
+
+    df.to_csv(output_path, index=False)
+
+if __name__ == "__main__":
+    clean_phishing(
+        "data/raw/phishing.csv",
+        "data/processed/phishing_clean.csv"
+    )
